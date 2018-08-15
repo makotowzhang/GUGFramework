d586460fa82172e408e512d7fd6ffcd7e1fe7f41
makotowzhang <31465825+makotowzhang@users.noreply.github.com>
2018-08-15 10:53:48 +0800

    Development

---
+++ GUGFramework/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GUGFramework
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
