a368583091a49ee69efdcd63281566bb7d8d4cfe
makotowzhang <31465825+makotowzhang@users.noreply.github.com>
2018-09-05 21:27:16 +0800

    desc

---
+++ Model/SystemModel/MenuModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+
+namespace Model.SystemModel
+{
+    public class MenuModel
+    {
+        public Guid Id { get; set; }
+        public string MenuName { get; set; }
+        public string MenuUrl { get; set; }
+        public Guid? ParentId { get; set; }
+        public string CreateUser { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public string UpdateUser { get; set; }
+        public DateTime? UpdateTime { get; set; }
+    }
+}
---
+++ Model/SystemModel/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.SystemModel
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string TrueName { get; set; }
+        public bool? IsEnabled { get; set; } = true;
+        public bool? IsDel { get; set; } = false;
+        public List<string> UserRole { get; set; }
+        public Guid? CreateUser { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public Guid? UpdateUser { get; set; }
+        public DateTime? UpdateTime { get; set; }
+    }
+}
